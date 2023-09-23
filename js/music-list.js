d6e82a1246f9491f0bc6f70a8507bd4d38abb699
Debamit Dasgupta <95354270+DebamitDasgupta@users.noreply.github.com>
2023-09-23 01:10:33 -0700

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -23,8 +23,8 @@ let allMusic = [
     src: "music-2"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
+    name: "Love Niwatte",
+    artist: "CK love",
     img: "music-3",
     src: "music-3"
   },
@@ -45,6 +45,102 @@ let allMusic = [
     artist: "Jasoull dullo",
     img: "music-6",
     src: "music-6"
+  },
+  {
+    name: "Build me a Bitch",
+    artist: "Bella porch",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "Brown Munde",
+    artist: "AP Dhillon",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Ola ye Kale kale",
+    artist: "Sofia reyes",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "What Jhumka ?",
+    artist: "Arijit shing",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Pyaar hota pheli baar",
+    artist: "arijit singh",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "Gallan Gooriyan",
+    artist: "song album",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "Laung da Lashkara",
+    artist: "Patiyala House",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Tere Pyaar mein",
+    artist: "Arijit singh",
+    img: "music-18",
+    src: "music-18",
+  },
+  {
+    name: "One two three four get on the dance floor",
+    artist: "Chennai Express",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Chak Chaka",
+    artist: "Sonali thakur",
+    img: "music-20",
+    src: "music-20",
+  },
+  {
+    name: "Param Sundari",
+    artist: "A.R Rhaman",
+    img: "music-21",
+    src: "music-21",
+  },
+  {
+    name: "Let's Nacho",
+    artist: "Song Album",
+    img: "music-22",
+    src: "music-22",
+  },
+  {
+    name: "Buddhu Sa Mann",
+    artist: "song album",
+    img: "music-23",
+    src: "music-23",
+  },
+  {
+    name: "Ladki Kar Gayi Chul",
+    artist: "song album",
+    img: "music-24",
+    src: "music-24",
+  },
+  {
+    name: "Dancin",
+    artist: "Krono",
+    img: "music-25",
+    src: "music-25",
+  },
+  {
+    name: "Starboy ft.Weekend",
+    artist: "Weekend",
+    img: "music-26",
+    src: "music-26",
   },
   // like this paste it and remember to give comma after ending of this bracket }
   // {
